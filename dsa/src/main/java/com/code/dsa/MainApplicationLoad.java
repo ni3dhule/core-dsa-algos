ce78ca5a7640d2da8d5e967f34babb1f1487c7f6
niteen <ndhule1988@gmail.com>
2022-09-13 23:37:12 +0530

    Math max function code checkin

---
+++ dsa/src/main/java/com/code/dsa/MainApplicationLoad.java
@@ -0,0 +1,15 @@
+package com.code.dsa;
+
+import com.code.util.MathMaxUtil;
+
+public class MainApplicationLoad {
+	
+	public static void init() {
+		System.out.println("Main Application Loading Started ");
+		int iFirst = 10;
+		int iSecond = 30;
+		int iMax = MathMaxUtil.findMax(iFirst, iSecond);
+		System.out.println("Maximum Number is : "+iMax);
+	}
+
+}
---
+++ dsa/src/main/java/com/code/util/MathMaxUtil.java
@@ -0,0 +1,9 @@
+package com.code.util;
+
+public class MathMaxUtil {
+
+	public static int findMax(int iFirst, int iSecond) {
+		return Math.max(iFirst, iSecond);
+	}
+	
+}
