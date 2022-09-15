67c3826da26dab69e2e4367f37ddf94ddd2bdc05
niteen <ndhule1988@gmail.com>
2022-09-15 23:49:51 +0530

    Math other functions like i. round - round off function ii. log - logarithmic function iii. pow  - power function

--- dsa/src/main/java/com/code/dsa/MainApplicationLoad.java
+++ dsa/src/main/java/com/code/dsa/MainApplicationLoad.java
@@ -13,6 +13,17 @@ public class MainApplicationLoad {
 		
 		int iMin = MathMaxUtil.findMin(iFirst, iSecond);
 		System.out.println("Minimum Number is : "+iMin);
+		
+		double x = -83.76;  
+        // find the closest int for the double  
+        System.out.println("Round of "+x+" is : "+Math.round(x)); 
+        
+        x = 28;    
+        double y = 4;  
+        
+        System.out.println("Logarithm of x is: " + Math.log(x));   
+        System.out.println("Logarithm of y is: " + Math.log(y)); 
+        System.out.println("Power of x and y is: " + Math.pow(x, y));
 	}
 
 }
