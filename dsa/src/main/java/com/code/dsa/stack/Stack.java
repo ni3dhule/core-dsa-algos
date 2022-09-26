2e64dabfa72c84c9a77b6642af8e91634286e7f2
niteen_dhule <ndhule1988@gmail.com>
2022-09-26 22:45:04 +0530

    Creating the user defined stack added new method to stack

--- dsa/src/main/java/com/code/dsa/stack/Stack.java
+++ dsa/src/main/java/com/code/dsa/stack/Stack.java
@@ -3,5 +3,12 @@ package com.code.dsa.stack;
 public class Stack {
     int MAX_STACK_SIZE = 5;
     int[] array = new int[MAX_STACK_SIZE];
-
+    int iTop = -1;
+    public void push(int iItem){
+        if(array.length == MAX_STACK_SIZE-1){
+            System.out.println("Stack is overflow !!!");
+            return;
+        }
+        array[++iTop] = iItem;
+    }
 }
