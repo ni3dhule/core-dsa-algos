83bdcef9296ed8005c2753d8f49e358fab8ed59b
niteen <ndhule1988@gmail.com>
2023-09-26 00:27:32 +0530

    Adding Custom Queue data structure.

--- dsa/src/main/java/com/code/dsa/queue/Queue.java
+++ dsa/src/main/java/com/code/dsa/queue/Queue.java
@@ -1,4 +1,38 @@
 package com.code.dsa.queue;
 
 public class Queue {
+    // queue works on FIFO mechanism
+    //queue methods that we need to implement
+
+    // add(item)
+    // add(index, item)
+    // addAll(index, collection)
+    // size()
+    // clear()
+    // remove()
+    // remove(index)
+    // remove(item)
+    // get(index)
+    // set(index, item)
+    // indexOf(item)
+    // lastIndexOf(item)
+    // isEmpty()
+    // hashCode()
+    // equals(item)
+    // contains(item)
+    // containsAll(collection)
+    // sort(comparator)
+    // offer(item)
+    // poll()
+    // element()
+    // peek()
+
+
+
+
+    public void add(int item) {
+        //Do later
+    }
+
+
 }
--- dsa/src/main/java/com/code/dsa/stack/Stack.java
+++ dsa/src/main/java/com/code/dsa/stack/Stack.java
@@ -1,12 +1,17 @@
 package com.code.dsa.stack;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Stack {
-    int MAX_STACK_SIZE = 5;
+    private static final int MAX_STACK_SIZE = 5;
     int[] array = new int[MAX_STACK_SIZE];
     int iTop = -1;
+
+    Logger logger = LoggerFactory.getLogger(Stack.class);
     public void push(int iItem){
         if(iTop == MAX_STACK_SIZE-1){
-            System.out.println("Stack is overflow !!!");
+            logger.info("Stack is overflow !!!");
             return;
         }
         array[++iTop] = iItem;
@@ -14,21 +19,18 @@ public class Stack {
 
     public int pop(){
         if(iTop == -1){
-            System.out.printf("Stack is empty, can't perform pop operation.");
+            logger.info("Stack is empty, can't perform pop operation.");
             return -1;
         }
-        int item = array[iTop--];
-        return item;
+        return array[iTop--];
     }
 
     public void show(){
-        System.out.println();
-        System.out.printf("Stack Elements : [ ");
+        logger.info("Stack Elements : [ ");
         for(int i=0; i <= iTop; i++){
-            System.out.printf(" "+array[i]);
+            logger.info(" {}", array[i]);
         }
-        System.out.printf(" ]");
-        System.out.println();
+        logger.info(" ]");
     }
 
 }
